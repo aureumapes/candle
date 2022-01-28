8f2f96386bdd66764ae07c72c21bd9e16ebca9f8
SomehowPatrick <somehowpatrick@protonmail.com>
2022-01-28 14:39:16 +0100

    Replaced if-else with switch & added > and < operations

--- if.go
+++ if.go
@@ -9,28 +9,43 @@ func If(command string) {
 	splitCmd := strings.Split(command, " ")
 	variable := getFloat(variables[splitCmd[1]])
 	number, _ := strconv.ParseFloat(splitCmd[3], 64)
-	if splitCmd[2] == "==" {
+
+	switch splitCmd[2] {
+	case "==":
 		if variable == number {
 			executeCommand := strings.Join(splitCmd[4:], " ")
 			Execute(executeCommand)
 		}
-	}
-	if splitCmd[2] == "!=" {
+		break
+	case "!=":
 		if variable != number {
 			executeCommand := strings.Join(splitCmd[4:], " ")
 			Execute(executeCommand)
 		}
-	}
-	if splitCmd[2] == ">=" {
+		break
+	case ">=":
 		if variable >= number {
 			executeCommand := strings.Join(splitCmd[4:], " ")
 			Execute(executeCommand)
 		}
-	}
-	if splitCmd[2] == "<=" {
+		break
+	case "<=":
 		if variable <= number {
 			executeCommand := strings.Join(splitCmd[4:], " ")
 			Execute(executeCommand)
 		}
+		break
+	case "<":
+		if variable < number {
+			executeCommand := strings.Join(splitCmd[4:], " ")
+			Execute(executeCommand)
+		}
+		break
+	case ">":
+		if variable > number {
+			executeCommand := strings.Join(splitCmd[4:], " ")
+			Execute(executeCommand)
+		}
+		break
 	}
 }
