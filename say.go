1cd2e2dc64f5758f36b0451671cf8fda7945c7b6
SomehowPatrick <somehowpatrick@protonmail.com>
2022-01-28 12:01:29 +0100

    Cleanup and typo fixing

---
+++ say.go
@@ -0,0 +1,18 @@
+// By AureumApes
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Say(command string) {
+	splitCmd := strings.Split(command, " ")
+	_, ok := variables[splitCmd[1]]
+	if ok {
+		fmt.Println(variables[splitCmd[1]])
+	} else {
+		fmt.Println(splitCmd[1] + ": Unknown Variable")
+	}
+}
---
+++ set.go
@@ -0,0 +1,25 @@
+// By AureumApes
+
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func Set(command string) {
+	splitCmd := strings.Split(command, " ")
+	if splitCmd[2] == "to" {
+		if isNumeric(splitCmd[3]) {
+			content, _ := strconv.ParseFloat(splitCmd[3], 64)
+			variables[splitCmd[1]] = content
+		} else {
+			variables[splitCmd[1]] = splitCmd[3]
+		}
+	}
+}
+
+func isNumeric(s string) bool {
+	_, err := strconv.ParseFloat(s, 64)
+	return err == nil
+}
